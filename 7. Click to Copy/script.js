350467c1aaa30623c31bed20d941bc45a8dc9498
Animesh Seth <sethanimesh@hotmail.com>
2023-06-11 19:02:36 +0530

    copy text project file

---
+++ 7. Click to Copy/script.js
@@ -0,0 +1,24 @@
+const btn = document.querySelector(".btn");
+const coupon = document.querySelector(".coupon");
+
+const copyText = (e) =>{
+    e.preventDefault();
+
+    // coupon.select();
+    // coupon.setSelectionRange(0, 999999);
+    // document.execCommand("copy");
+    navigator.clipboard.writeText(coupon.value)
+        .then(()=>{
+            btn.textContent = "Copied!!!";     
+            setTimeout(()=>{
+                btn.textContent = "Copy";
+            }, 3000);
+        })
+
+    // btn.textContent = "Copied!!!";
+    // setTimeout(()=>{
+    //     btn.textContent = "Copy";
+    // }, 3000);
+}
+
+btn.addEventListener("click", copyText);
